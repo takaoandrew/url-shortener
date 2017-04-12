e8c4ae4344aeaefa1c191ceb91b7bdf65fa5c4c3
Andrew Takao <takaoandrew@gmail.com>
2017-04-12 22:08:39 +0000

    Add first draft

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express')
+var opn = require('opn')
 var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var openurl = require('openurl')
+var url
+var http = require('http')
+app.get('/1000', function (req, res) {
+  openurl(url) 
+  res.send(url)
+});
+app.get('/:query', function (req, res) {
+  url = req.params.query
+  openurl.open(url)
+  res.end(url)
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 8080!')
 })
